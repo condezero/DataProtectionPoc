929b06f4976fb8e45963c941c6e8c026a5a14e57
Carlos Lisandra Osborne <carlos.lisandra@toolfactory.net>
2020-12-16 10:36:58 +0100

    start commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DataProtectionPoc
 Proof of concept using IDataProtectionProvider
+
+## Usage
+
+### Protect
+```bash
+http://localhost:5000/protect/[string to protect]
+```
+
+### Unprotect
+```bash
+http://localhost:5000/unprotect/[string protected]
+```
+
+
---
+++ src/ApiService/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ApiService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddDataProtection()
+                                .UseCustomCryptographicAlgorithms(
+                                new ManagedAuthenticatedEncryptorConfiguration()
+                                {
+                                    EncryptionAlgorithmType = typeof(Aes),
+                                    EncryptionAlgorithmKeySize = 256,
+                                    ValidationAlgorithmType = typeof(HMACSHA256)
+                                });
+                        // IDataProtector
+                        services.AddSingleton(c => {
+                            var protectionProvider = c.GetRequiredService<IDataProtectionProvider>();
+                            return protectionProvider.CreateProtector("myTestPrupose");
+                        });
+                     
+                    });
+                    webBuilder.Configure(app => {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapGet("/protect/{value}", async context =>
+                            {
+                                var requestedValue = (string)context.Request.RouteValues["value"];
+                                var protectionProvider = context.Request.HttpContext.RequestServices.GetRequiredService<IDataProtector>();
+                                await context.Response.WriteAsync(protectionProvider.Protect(requestedValue));
+                            });
+                            endpoints.MapGet("/unprotect/{value}", async context =>
+                            {
+                                var requestedValue = (string)context.Request.RouteValues["value"];
+                                var protectionProvider = context.Request.HttpContext.RequestServices.GetRequiredService<IDataProtector>();
+                                await context.Response.WriteAsync(protectionProvider.Unprotect(requestedValue));
+                            });
+                        });
+                    });
+                });
+    }
+}
